eecc5d0cfa063e5089be964d7d8ce8dad993599e
Benson46 <bensonbaiju46@gmail.com>
2024-09-05 10:21:56 +0530

    initial stage basics completed and session handelled

---
+++ app.js
@@ -0,0 +1,15 @@
+//mongodb connecting
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/user_management_system");
+
+//express
+const express = require('express');
+const app = express();
+
+
+//user routes
+const user_Route = require('./routes/userRoutes')
+app.use('/',user_Route);
+
+//app listening
+app.listen(3000,()=>console.log('http://localhost:3000'))
